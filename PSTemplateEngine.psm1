267d06c42d6b0054e923540645328ba4a8b8d585
ChrisMagnuson <hourglasssand@gmail.com>
2016-04-02 14:37:41 -0400

    Initial commit

---
+++ PSTemplateEngine.psm1
@@ -0,0 +1,20 @@
+﻿#Requires -Version 5
+fucntion Invoke-ProcessTemplate {
+    param(
+        [Parameter(Mandatory=$true)]$Template
+        #,$Data
+    )
+
+    $TemplateAsSingleString = Get-Content $Template | Out-String
+    $TemplateHereString = @"
+@"
+$TemplateAsSingleString
+"`@
+"@
+
+    $TemplateAfterProcessing = Invoke-Expression $TemplateHereString
+
+    Compare-Object $($TemplateAsSingleString -split '\n') $($TemplateAfterProcessing -split '\n') | fl * | Out-String -stream | Write-Debug
+
+    $TemplateAfterProcessing
+}
